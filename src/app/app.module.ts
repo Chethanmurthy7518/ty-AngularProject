09bf940e4895e5d42c43ebc6c36be833bf86d0c8
Chethan <chethanmurthy7518@gmail.com>
2021-10-11 16:49:51 +0530

    Commiting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Basic } from './basic.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Basic,
+    LoginComponent
+    
   ],
   imports: [
     BrowserModule
---
+++ src/app/basic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "basic",
+    template:"<h1>Welcome to Angular</h1>",
+    styles:["h1{color:red}"]
+})
+export class Basic{
+    
+}
+
